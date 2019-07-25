0d6ed2f45656eb54563186cc7189afac98a6340a
Downground <thomazmancini.tm@gmail.com>
2019-07-25 10:12:19 -0300

    finalizacao do projeto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SigninComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
-    declarations: [ SigninComponent ],
+    declarations: [ 
+        SigninComponent,
+        SignupComponent,
+        HomeComponent
+    ],
     imports: [ 
             CommonModule,
             ReactiveFormsModule,
             VMessageModule,
-            RouterModule
+            RouterModule,
+            FormsModule
         ]
 })
 export class HomeModule {
